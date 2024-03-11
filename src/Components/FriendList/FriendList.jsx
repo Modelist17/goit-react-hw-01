fdbffe4fc54461cec54250bc75ddade7c3c164bd
Modelist17 <hunteruboat@gmail.com>
2024-03-11 12:48:01 +0100

    changed

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -7,9 +7,9 @@ import styles from './FriendList.module.css';
 const FriendList = ({ friends }) => {
   return (
     <ul className={styles.friendList}>
-      {friends.map((friend) => (
-        <li key={friend.id}>
-          <FriendListItem {...friend} />
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <FriendListItem id={id} avatar={avatar} name={name} isOnline={isOnline} />
         </li>
       ))}
     </ul>
