e906097d421a19cff3750e0136a578f8a8a28334
Modelist17 <hunteruboat@gmail.com>
2024-03-14 19:04:19 +0100

    maybe baby

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,34 +1,18 @@
 import React from "react";
 import css from "./FriendList.module.css";
 import clsx from "clsx";
+import FriendListItem from "../FriendListItem/FriendListItem"; // Імпорт компонента FriendListItem
 
 const FriendList = ({ friends }) => {
   const classes = [css.friendList];
 
   return (
     <ul className={clsx(...classes)}>
-      {friends.map(({ name, avatar, isOnline, id }) => {
-        const isFriendOnline = isOnline === true;
-
-        return (
-          <li
-            className={clsx(css.friendListCard, {
-              [css.online]: isFriendOnline,
-            })}
-            key={id}
-          >
-            <div>
-              <img src={avatar} alt="Avatar" width="48" />
-              <p className={css.friendListText}>{name}</p>
-              {isFriendOnline ? (
-                <p className={css.online}>{isOnline}Online</p>
-              ) : (
-                <p className={css.ofline}>{isOnline}Offline</p>
-              )}
-            </div>
-          </li>
-        );
-      })}
+      {friends.map(({ name, avatar, isOnline, id }) => (
+        <li key={id}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
     </ul>
   );
 };
